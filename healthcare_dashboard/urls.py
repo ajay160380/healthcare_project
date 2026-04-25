84d85444d3fb0d01f37dc14ed72e5e8a73eb0eda
ajay160380 <ajaykumar160380@gmail.com>
2026-04-25 22:40:12 +0530

    Updated login UI

--- healthcare_dashboard/urls.py
+++ healthcare_dashboard/urls.py
@@ -4,10 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
 urlpatterns = [
-path('admin/', admin.site.urls),
-path('', include('accounts.urls')),
-path('patients/', include('patients.urls', namespace='patients')),
-path('doctors/', include('doctors.urls', namespace='doctors')),
-path('appointments/', include('appointments.urls', namespace='appointments')),
-path('pharmacy/', include('pharmacy.urls', namespace='pharmacy')),
+    path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
+    path('patients/', include('patients.urls', namespace='patients')),
+    path('doctors/', include('doctors.urls', namespace='doctors')),
+    path('appointments/', include('appointments.urls', namespace='appointments')),
+    path('pharmacy/', include('pharmacy.urls', namespace='pharmacy')),
+    # Redirect old/incorrect login URLs to the correct login page
+    path('dashboard/login', RedirectView.as_view(url='/accounts/login/', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
